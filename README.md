a135a3c7a768ecc2461f9480b5b8be959a855cbf
lu-annie <109433768+lu-annie@users.noreply.github.com>
2023-01-16 10:58:51 -0800

    Add files via upload

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 A Pomodoro timer that keeps you focused on your work by showing you a boring picture of a pomegranate every time you open a new tab.
 
 Sources:
-https://codepen.io/TLJens/pen/azedap
+https://codepen.io/rajdgreat007/pen/edvZpx
+
 https://www.youtube.com/watch?v=dIrXIJ781DQ
+
 https://freshman.tech/first-chrome-extension/
 
--- popup.js
+++ popup.js
@@ -1,34 +1,85 @@
-alert("Your Pomogranate study session has begun!")
-
-var interval;
-
-function countdown() {
-  clearInterval(interval);
-  interval = setInterval( function() {
-      var timer = $('.js-timeout').html();
-      timer = timer.split(':');
-      var minutes = timer[0];
-      var seconds = timer[1];
-      seconds -= 1;
-      if (minutes < 0) return;
-      else if (seconds < 0 && minutes != 0) {
-          minutes -= 1;
-          seconds = 59;
+var pomodoro = {
+  started : false,
+  minutes : 0,
+  seconds : 0,
+  fillerHeight : 0,
+  fillerIncrement : 0,
+  interval : null,
+  minutesDom : null,
+  secondsDom : null,
+  fillerDom : null,
+  init : function(){
+    var self = this;
+    this.minutesDom = document.querySelector('#minutes');
+    this.secondsDom = document.querySelector('#seconds');
+    this.fillerDom = document.querySelector('#filler');
+    this.interval = setInterval(function(){
+      self.intervalCallback.apply(self);
+    }, 1000);
+    document.querySelector('#work').onclick = function(){
+      self.startWork.apply(self);
+    };
+    document.querySelector('#shortBreak').onclick = function(){
+      self.startShortBreak.apply(self);
+    };
+    document.querySelector('#longBreak').onclick = function(){
+      self.startLongBreak.apply(self);
+    };
+    document.querySelector('#stop').onclick = function(){
+      self.stopTimer.apply(self);
+    };
+  },
+  resetVariables : function(mins, secs, started){
+    this.minutes = mins;
+    this.seconds = secs;
+    this.started = started;
+    this.fillerIncrement = 200/(this.minutes*60);
+    this.fillerHeight = 0;  
+  },
+  startWork: function() {
+    this.resetVariables(25, 0, true);
+  },
+  startShortBreak : function(){
+    this.resetVariables(5, 0, true);
+  },
+  startLongBreak : function(){
+    this.resetVariables(15, 0, true);
+  },
+  stopTimer : function(){
+    this.resetVariables(25, 0, false);
+    this.updateDom();
+  },
+  toDoubleDigit : function(num){
+    if(num < 10) {
+      return "0" + parseInt(num, 10);
+    }
+    return num;
+  },
+  updateDom : function(){
+    this.minutesDom.innerHTML = this.toDoubleDigit(this.minutes);
+    this.secondsDom.innerHTML = this.toDoubleDigit(this.seconds);
+    this.fillerHeight = this.fillerHeight + this.fillerIncrement;
+    this.fillerDom.style.height = this.fillerHeight + 'px';
+  },
+  intervalCallback : function(){
+    if(!this.started) return false;
+    if(this.seconds == 0) {
+      if(this.minutes == 0) {
+        this.timerComplete();
+        return;
       }
-      else if (seconds < 10 && length.seconds != 2) seconds = '0' + seconds;
-
-      $('.js-timeout').html(minutes + ':' + seconds);
-
-      if (minutes == 0 && seconds == 0) clearInterval(interval);
-  }, 1000);
-}
-
-$('#js-startTimer').click(function () {
-  $('.js-timeout').text("25:00");
-  countdown();
-});
-
-$('#js-resetTimer').click(function () {
-  $('.js-timeout').text("25:00");
-  clearInterval(interval);
-});
+      this.seconds = 59;
+      this.minutes--;
+    } else {
+      this.seconds--;
+    }
+    this.updateDom();
+  },
+  timerComplete : function(){
+    this.started = false;
+    this.fillerHeight = 0;
+  }
+};
+window.onload = function(){
+pomodoro.init();
+};
